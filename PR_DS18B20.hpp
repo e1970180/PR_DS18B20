3b6914300bc459159734356850f5ea693f3a84ab
Alex Pro <e1970180@ya.ru>
2018-12-25 12:08:21 +0300

    Update PR_DS18B20.hpp

--- PR_DS18B20.hpp
+++ PR_DS18B20.hpp
@@ -7,92 +7,66 @@
 
 #include "PR_GetTau.hpp"
 
-#ifndef	PR_DS18B20_PRECISION_DEFAULT
-	#define	PR_DS18B20_PRECISION_DEFAULT		9		//[9...12]
-#endif
-
 #ifndef	PR_DS18B20_UPDATE_PERIOD_DEF
-	#define	PR_DS18B20_UPDATE_PERIOD_DEF		15		//[ms]
+	#define	PR_DS18B20_UPDATE_PERIOD_DEF			15		//[ms]
 #endif
 
 #ifndef	PR_DS18B20_INVALID_TEMPERATURE_VALUE
-	#define	PR_DS18B20_INVALID_TEMPERATURE_VALUE	-127	//[degC] returned value if no correct measured temperature available 
+	#define PR_DS18B20_INVALID_TEMPERATURE_VALUE	123		//[degC] returned value if no correct measured temperature available 
 #endif
 
-#define	DS18B20_CONV_TIME						2000	//[ms]
-
-class PR_DS18B20 {
+class PR_DS18B20 : public DallasTemperature {
         public:
-			PR_DS18B20( const uint8_t pin, const uint8_t resolution = PR_DS18B20_PRECISION_DEFAULT, 
-						const int16_t invalidTemp = PR_DS18B20_INVALID_TEMPERATURE_VALUE );
+			PR_DS18B20(const uint8_t pin, const uint32_t updatePeriod = PR_DS18B20_UPDATE_PERIOD_DEF);
             
-			void 	setResolution(const uint8_t resolution);
 			void	setUpdatePeriod(const uint32_t updatePeriod);
 			
-			void	loop();
-			
-			float 	get();
+			float	loop();
+			float 	getC();
 			
         protected:
-    		OneWire                 oneWire;    			//for shadow  temp sensor
-			DallasTemperature       sensors;       		// Pass our oneWire reference to Dallas Temperature. 
-			
-			uint32_t				_lastTime;
-			float 					_lastMeasuredTemp;
-			int16_t					_invalidTemp;
-			uint32_t				_updatePeriod;		//[ms]
+			OneWire         _oneWire;
+			float 			_lastMeasuredTemp;
+			uint32_t		_updatePeriod;			//[ms]
 			
 };
 
-PR_DS18B20::PR_DS18B20( const uint8_t pin, const uint8_t resolution, const int16_t invalidTemp )
-	:	oneWire(pin), 
-		sensors(&oneWire),		// Pass our oneWire reference to Dallas Temperature. 
-		_invalidTemp(invalidTemp)
-	{
-	sensors.begin();
-	sensors.setResolution(resolution);
-	sensors.setWaitForConversion(false);	//switch on async mode
+PR_DS18B20::PR_DS18B20(const uint8_t pin, const uint32_t updatePeriod) : DallasTemperature() , _oneWire(pin) {
+	
+	setOneWire(&_oneWire);
+	begin();
+	setWaitForConversion(false);		//switch on async mode
 
-	_lastMeasuredTemp = _invalidTemp;
-	_updatePeriod = PR_DS18B20_UPDATE_PERIOD_DEF;
+	_lastMeasuredTemp = PR_DS18B20_INVALID_TEMPERATURE_VALUE;
+	_updatePeriod = updatePeriod;
 }
-
-void 	PR_DS18B20::setResolution(const uint8_t resolution ) {
-
-	sensors.setResolution(resolution);
-}   
 
 void	PR_DS18B20::setUpdatePeriod(const uint32_t updatePeriod) {	_updatePeriod = updatePeriod; }
 
-float 	PR_DS18B20::get()	{	return _lastMeasuredTemp;	}
+float 	PR_DS18B20::getC()	{	return _lastMeasuredTemp;	}
 
-void	PR_DS18B20::loop() {	
+float	PR_DS18B20::loop() {	
 
-	typedef enum TempReqState {
-		REQUESTED			= 2,
-		READY 				= 1, 
-		IDLE 				= 0 
-	} TempReqState_t;
-
-	//request and read temp asynchronous
-	static TempReqState_t state = IDLE;
+	static bool 	isRequested = false;
+	static uint32_t	_lastTime = 0;
 	
-	switch (state) {
-		case IDLE:
-			if ( PR_getTauMS(_lastTime) > _updatePeriod ) {
-				sensors.requestTemperatures();
+	if (isRequested) {
+		if ( PR_getTauMS(_lastTime) > millisToWaitForConversion(getResolution()) ) {
+				
+				_lastMeasuredTemp = getTempCByIndex(0);
+				if (_lastMeasuredTemp == DEVICE_DISCONNECTED_C) _lastMeasuredTemp = PR_DS18B20_INVALID_TEMPERATURE_VALUE;		
 				_lastTime = PR_getNowMS();
-				state = REQUESTED;
+				isRequested = false;
 			}
-		break;
-		case REQUESTED:
-			if ( PR_getTauMS(_lastTime) > DS18B20_CONV_TIME ) {
-				_lastMeasuredTemp = sensors.getTempCByIndex(0);
-				_lastTime = PR_getNowMS();
-				state = IDLE;
-			}
-		break;
 	}
+	else {
+		if ( PR_getTauMS(_lastTime) > _updatePeriod ) {
+				requestTemperatures();
+				_lastTime = PR_getNowMS();
+				isRequested = true;
+		}
+	}
+	return _lastMeasuredTemp;
 }
 
 
